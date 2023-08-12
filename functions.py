301c774e42725fbb69f4c288094edf4d46ff00d0
norxondor_gorgonax <axel.brisse@gmail.com>
2023-08-12 13:20:23 +0200

    clean and test linear_combination

--- functions.py
+++ functions.py
@@ -2,10 +2,8 @@ from classes import Vector
 
 
 def linear_combination(vecs, coefs):
+    assert vecs
     assert len(vecs) == len(coefs)
-    if len(vecs) == 0:
-        return Vector([])
-    assert all(len(x) == len(vecs[0]) for x in vecs)
-    return sum(
-        [vec * coef for vec, coef in zip(vecs, coefs)], Vector([0] * len(vecs[0]))
-    )  # TODO why do we need a starting value?
+    size = len(vecs[0])
+    assert all(len(x) == size for x in vecs)
+    return sum(map(Vector.__mul__, vecs, coefs), Vector([0] * size))
--- test_functions.py
+++ test_functions.py
@@ -1,11 +1,9 @@
+import pytest
 from classes import Vector
 from functions import linear_combination
 
 
 def test_linear_combination_valid():
-    assert linear_combination([], []) == Vector([])
-    assert linear_combination([Vector([1, 2, 3])], [-1]) == Vector([-1, -2, -3])
-    assert linear_combination([Vector([1, 2, 3])], [0]) == Vector([0, 0, 0])
     assert linear_combination([Vector([1, 2, 3])], [1]) == Vector([1, 2, 3])
     assert linear_combination([Vector([1, 2, 3])], [2]) == Vector([2, 4, 6])
     assert linear_combination([Vector([1, 2, 3]), Vector([4, 5, 6])], [7, 8]) == Vector(
@@ -14,3 +12,17 @@ def test_linear_combination_valid():
     assert linear_combination(
         [Vector([1, 2, 3]), Vector([2, 4, 6])], [2, -1]
     ) == Vector([0, 0, 0])
+    assert linear_combination(
+        [Vector([0, 0]), Vector([1, 1]), Vector([2, 2])], [1, 1, 1]
+    ) == Vector([3, 3])
+
+
+def test_linear_combination_invalid():
+    with pytest.raises(Exception):
+        linear_combination([Vector([1])], [1, 1])
+    with pytest.raises(Exception):
+        linear_combination([Vector([1]), Vector([1])], [1])
+    with pytest.raises(Exception):
+        linear_combination([Vector([1]), Vector([1, 1])], [1, 1])
+    with pytest.raises(Exception):
+        linear_combination([], [])
