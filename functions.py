68962c52046bc62549a409536210d8c7a19cbd65
norxondor_gorgonax <axel.brisse@gmail.com>
2023-08-12 16:05:47 +0200

    comments in functions.py

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ from classes import Vector
 
 
 def linear_combination(vecs, coefs):
-    assert vecs
+    assert vecs  # if the list is empty, we can't determine the size of the zero vector to return
     assert len(vecs) == len(coefs)
     size = len(vecs[0])
     assert all(len(x) == size for x in vecs)
@@ -11,5 +11,5 @@ def linear_combination(vecs, coefs):
 
 def lerp(u, v, t):
     assert type(u) == type(v)
-    assert 0 <= t <= 1
+    assert 0 <= t <= 1  # the subject is not clear on this point
     return u * (1 - t) + v * t
