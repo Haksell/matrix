4b8d79b4a70eb6408ee8a050af8dd4e0a7d5c953
Axel Brisse <axel.brisse@gmail.com>
2024-05-26 10:58:38 +0200

    start testing prime field

--- prime_field.py
+++ prime_field.py
@@ -1,12 +1,22 @@
-from functools import wraps
+from functools import cache, wraps
 from math import isqrt
+
+
+@cache
+def _is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    return all(n % i != 0 and n % (i + 2) != 0 for i in range(5, isqrt(n) + 1, 6))
 
 
 class PrimeField:
     def __init__(self, n, prime):
         assert isinstance(prime, int)
-        assert prime >= 2
-        assert all(prime % i for i in range(2, isqrt(prime) + 1))
+        assert _is_prime(prime)
         assert isinstance(n, int)
         assert 0 <= n < prime
         self.n = n
---
+++ test_prime_field.py
@@ -0,0 +1,57 @@
+import math
+import pytest
+from prime_field import PrimeField
+
+
+def test_init():
+    with pytest.raises(AssertionError):
+        PrimeField(12, 7)
+    with pytest.raises(AssertionError):
+        PrimeField(3, 6)
+    with pytest.raises(AssertionError):
+        PrimeField(math.pi, 7)
+    with pytest.raises(AssertionError):
+        PrimeField(2, math.pi)
+
+
+def test_cmp():
+    assert PrimeField(3, 7) == PrimeField(3, 7)
+    assert not (PrimeField(3, 7) != PrimeField(3, 7))
+    assert not (PrimeField(3, 7) < PrimeField(3, 7))
+    assert PrimeField(3, 7) <= PrimeField(3, 7)
+    assert not (PrimeField(3, 7) > PrimeField(3, 7))
+    assert PrimeField(3, 7) >= PrimeField(3, 7)
+    assert not (PrimeField(3, 7) == PrimeField(5, 7))
+    assert PrimeField(3, 7) != PrimeField(5, 7)
+    assert PrimeField(3, 7) < PrimeField(5, 7)
+    assert PrimeField(3, 7) <= PrimeField(5, 7)
+    assert not (PrimeField(3, 7) > PrimeField(5, 7))
+    assert not (PrimeField(3, 7) >= PrimeField(5, 7))
+    assert not (PrimeField(5, 7) == PrimeField(3, 7))
+    assert PrimeField(5, 7) != PrimeField(3, 7)
+    assert not (PrimeField(5, 7) < PrimeField(3, 7))
+    assert not (PrimeField(5, 7) <= PrimeField(3, 7))
+    assert PrimeField(5, 7) > PrimeField(3, 7)
+    assert PrimeField(5, 7) >= PrimeField(3, 7)
+    with pytest.raises(AssertionError):
+        assert PrimeField(3, 7) != PrimeField(5, 11)
+
+
+def test_add():
+    assert PrimeField(2, 7) + PrimeField(0, 7) == PrimeField(2, 7)
+    assert PrimeField(2, 7) + PrimeField(2, 7) == PrimeField(4, 7)
+    assert PrimeField(2, 7) + PrimeField(4, 7) == PrimeField(6, 7)
+    assert PrimeField(2, 7) + PrimeField(6, 7) == PrimeField(1, 7)
+    pf = PrimeField(2, 7)
+    pf += PrimeField(3, 7)
+    assert pf == PrimeField(5, 7)
+
+
+def test_sub():
+    assert PrimeField(2, 7) - PrimeField(0, 7) == PrimeField(2, 7)
+    assert PrimeField(2, 7) - PrimeField(2, 7) == PrimeField(0, 7)
+    assert PrimeField(2, 7) - PrimeField(4, 7) == PrimeField(5, 7)
+    assert PrimeField(2, 7) - PrimeField(6, 7) == PrimeField(3, 7)
+    pf = PrimeField(2, 7)
+    pf -= PrimeField(3, 7)
+    assert pf == PrimeField(6, 7)
